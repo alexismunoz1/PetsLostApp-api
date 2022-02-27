3862e041521311466eabad2c0b5f44dc2ca986c9
alexis muñoz <munozmiguelalexis@gmail.com>
2022-02-27 12:52:20 -0300

    add others configurations

--- server/index.ts
+++ server/index.ts
@@ -20,22 +20,21 @@ import { authMiddlewares } from "./controllers/middlewares";
 
 const app = express();
 
-// var whitelist = ["http://127.0.0.1:8080"];
-// var corsOptions = {
-//    origin: function (origin, callback) {
-//       if (whitelist.indexOf(origin) !== -1) {
-//          callback(null, true);
-//       } else {
-//          callback(new Error("Not allowed by CORS"));
-//       }
-//    },
+var whitelist = ["http://127.0.0.1:8080"];
+var corsOptions = {
+   origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+         callback(null, true);
+      } else {
+         callback(new Error("Not allowed by CORS"));
+      }
+   },
+};
+
+// const corsOptions = {
+//    origin: "http://127.0.0.1:8080",
 //    optionsSuccessStatus: 200,
 // };
-
-const corsOptions = {
-   origin: "http://127.0.0.1:8080",
-   optionsSuccessStatus: 200,
-};
 
 // app.use(cors(corsOptions));
 
